9bcae8af1ff2a3b946823dcadd68888647a5b7b5
Sharon Gibbons <35768914+SharonNicG@users.noreply.github.com>
2018-04-07 15:49:17 +0100

    Create fibonacci.py
    Single file entry containing Python script for Exercises 1 and 2 from Topics 1 and 2 - including ord() function.
    
    Discussion Forum Text
    
    Topic 1 Exercise 1
    My name is Sharon, so the first and last letter of my name (S + N = 19 + 14) give the number 33. The 33rd Fibonacci number is 3,524,578. 
    
    Topic 2 Exercise 2
    My surname is Gibbons
    
    The first letter G is number 71
    
    The last letter s is number 115
    
    Fibonacci number 186 is 332825110087067562321196029789634457848
    
    ord() returns an integer value for a single Unicode character. 
    
    So if you run the program with the first letter of your surname changed from a capital to lower case character, you would get different values for the first character and the corresponding Fibonacci number. see below.
    
    My surname is gibbons
    
    The first letter g is number 103
    
    The last letter s is number 115
    
    Fibonacci number 218 is 1621140188992194444701881625761731807571877809

---
+++ fibonacci.py
@@ -0,0 +1,28 @@
+# Sharon Gibbons, 04-02-2018
+# A program that displays Fibonacci numbers using people's names.
+# https://github.com/ianmcloughlin/python-fib
+
+def fib(n):
+  """This function returns the nth Fibonacci number."""
+  i = 0
+  j = 1
+  n = n - 1
+
+  while n >= 0:
+    i, j = j, i + j
+    n = n - 1
+  
+  return i
+
+name = "Gibbons"
+first = name[0]
+last = name[-1]
+firstno = ord(first)
+lastno = ord(last)
+x = firstno + lastno
+
+ans = fib(x)
+print("My surname is", name)
+print("The first letter", first, "is number", firstno)
+print("The last letter", last, "is number", lastno)
+print("Fibonacci number", x, "is", ans)
