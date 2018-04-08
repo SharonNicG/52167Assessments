1925076d6de8c9ad14530e00f15f52c1c338b2f9
Sharon Gibbons <35768914+SharonNicG@users.noreply.github.com>
2018-04-08 11:58:52 +0100

    Update euler_5.py
    Updated program extracting greatest common divisor (GCD) into a separate function - to retain speed of output return and allow code to be pared back.

--- euler_5.py
+++ euler_5.py
@@ -1,38 +1,27 @@
-# Sharon Gibbons, 11-03-2018
+# Sharon Gibbons, 12-03-2018
 # Project Euler Problem 5: Smallest multiple
 # https://projecteuler.net/problem=5
 # https://www.programiz.com/python-programming/examples/lcm
 
 # Python program to find the least common multiple for a range of numbers
 
-# defines function
-def findlcm(x, y):
-
-    # identifies numbers that are greater than and lesser than input integers
-    if x > y:
-       a, b = x, y          # a = greater number, b = lesser number
-    else:
-       a, b = y, x
-    
-    # checks if a division of the greater by lesser number is even
-    r = a % b
-
-    while r != 0:   # loop to execute if a division of the greater by lesser number is not even
-        a, b = b, r
-        r = a % b
-    
-    # gcd is common factor of input integers
-    gcd = b
-
-    # least common multiple is the computation of the product of the input integers divided by gcd
+# defines findgcd function (find greatest common factor/divisor)
     # https://codility.com/media/train/10-Gcd.pdf
-    lcm = int(int(x * y) / int(gcd))
+def findgcd(x, y):
+    if x % y == 0:  # if statement to identify 
+        return y
+    else:
+        return findgcd(y, x % y)
 
-    # output
+# defines findlcm function (find least common multiple)
+    # least common multiple is the computation of the product of the input integers divided by gcd of the input integers
+    # https://codility.com/media/train/10-Gcd.pdf
+def findlcm(x, y):
+    lcm = int(int(x * y) // int(findgcd(x, y)))
     return lcm
 
 # sets variables and keyword argument
-l = range(1, 21)
+l = range(1,21)
 
 x = l[0]
 y = l[1]
